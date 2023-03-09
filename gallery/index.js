2a8d0a55a95b70edbe601c57b3ba127aaa78b6fa
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-03-09 11:28:50 +0000

    current push

--- gallery/index.js
+++ gallery/index.js
@@ -13,3 +13,35 @@ document.addEventListener("click", (e) => {
         dropdown.classList.remove("active")
     })
 })
+
+// Cms 
+
+
+
+const URL = `https://q5cqw5we.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22gallery%22%5D%20%7B%0A%20%20%22imageUrl%22%3A%20galleryImage.asset-%3Eurl%0A%7D%0A`;
+
+
+ 
+fetch(URL)
+.then((res) => res.json())
+.then(({ result }) => {
+    const grid = document.querySelector(".past-work .wrapper")
+
+    if (result.length > 0) {
+        grid.innerHTML = ""
+    }
+
+    result.forEach((result, index) => {
+        const gridItem = document.createElement("img")
+        gridItem.src = result.imageUrl
+        grid.appendChild(gridItem)
+        gridItem.classList.add("grid-item")
+    })
+    
+})
+
+
+
+
+
+
